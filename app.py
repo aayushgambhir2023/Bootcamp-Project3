27324c79b8dd0f7768617e2265a88f02d5f6e9a9
aayushgambhir2023 <aayushgambhir2021@gmail.com>
2024-03-04 16:44:07 -0500

    Updates

--- app.py
+++ app.py
@@ -3,57 +3,54 @@ from flask import Flask, jsonify, request, render_template, send_file
 from pymongo import MongoClient
 from bson import ObjectId
 import json
+from flask import Flask, jsonify
+
 
 app = Flask(__name__)
 
 client = MongoClient('mongodb+srv://city_toronto:project3@cluster0.gt72z8e.mongodb.net/')
 db = client['city_toronto']
-
-@app.route('/api/v1.0/profit_revenue_expense_data/<int:year>', methods=['GET'])
-def display_expense_data(year):
-    # Define the dictionary of collections for different years
-    collections = {
+ # Define the dictionary of collections for different years
+collections = {
         2019: db['pNl_program_2019'],
         2020: db['pNl_program_2020'],
         2021: db['pNl_program_2021'],
         2022: db['pNl_program_2022'],
         2023: db['pNl_program_2023']
-    }
+}
 
-    # Initialize an empty dictionary to store expense data
-    expense_data = {}
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type'
+    response.headers['Access-Control-Allow-Methods'] = 'GET'
+    return response
 
-    # Fetch expense data for the specified year from MongoDB
+@app.route('/api/v1.0/summarized_revenue_data/<int:year>', methods=['GET'])
+def summarized_revenue_data(year):
+    summarized_revenue_data = summarize_data(year, 'rev')
+    return jsonify(summarized_revenue_data)
+
+@app.route('/api/v1.0/summarized_expense_data/<int:year>', methods=['GET'])
+def summarized_expense_data(year):
+    summarized_expense_data = summarize_data(year, 'exp')
+    return jsonify(summarized_expense_data)
+
+@app.route('/api/v1.0/summarized_profit_data/<int:year>', methods=['GET'])
+def summarized_profit_data(year):
+    summarized_profit_data = summarize_data(year, f'res-{year}')
+    return jsonify(summarized_profit_data)
+
+def summarize_data(year, field):
+    summarized_data = {}
     data = list(collections[year].find())
 
-    # Convert ObjectId to string in each document
     for item in data:
-        item['_id'] = str(item['_id'])
+        program = item["Program"]
+        value = item.get(field, 0)
+        summarized_data[program] = summarized_data.get(program, 0) + value
 
-    # Filter data to include only expense-related information
-    year_expense_data = []
-    for item in data:
-        document_data = {
-            "Program": item["Program"],
-            "_id": str(item["_id"]),
-            "rev": item.get("rev", 0),  # Get revenue value or default to 0
-            "exp": item.get("exp", 0),  # Get expense value or default to 0
-            "res": item.get(f"res-{year}", 0)  # Get result value for the current year or default to 0
-        }
-        year_expense_data.append(document_data)
-
-    # Store the expense data for the specified year in the expense_data dictionary
-    expense_data[year] = year_expense_data
-
-    # # Write the expense data to a JSON file
-    # file_path = f'expense_data_{year}.json'
-    # with open(file_path, 'w') as json_file:
-    #     json.dump(expense_data, json_file)
-
-    # # Return the JSON data as a response
-    # return send_file(file_path, as_attachment=True)
-    return jsonify(expense_data)
-
+    return summarized_data
 
 @app.route("/")
 def welcome():
