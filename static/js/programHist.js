30810e90af656b565e42bae939e46db470bfd3b0
Girellil <64373104+Girellil@users.noreply.github.com>
2024-03-05 16:30:51 -0500

    Logic Commit
    Commit with Logic.
    Missing General Formatting and Details!

--- static/js/programHist.js
+++ static/js/programHist.js
@@ -1,7 +1,10 @@
-//Template for Event Listener Buttons
-
 //Select your menu ID
-let selectedMenu1 = "subMenu3";
+let selectedMenu1 = "submenu22";
+
+// create getJson function to retrieve response
+function getJson(url){
+    return d3.json(url);
+}
 
 //Event Listener to your main function
 document.addEventListener('DOMContentLoaded', function() {
@@ -12,36 +15,60 @@ document.addEventListener('DOMContentLoaded', function() {
         //hide the Menu
         document.querySelector('.side-menu').style.display = 'none';
         //Call Function
-        yourFunctionName();
+        //console.log("clicked") test
+        startGraph();
     });
 });
 
-//Function!
-function yourFunctionName(){
+//Initial Graph Function
+function startGraph(){
 
+    //Select D3 Area and clear Content
     let graphicArea = d3.select("#graphics-output");
     graphicArea.html("");
 
-    //Start of the function:
-    graphicArea.append("div").attr("id", "left-column").style("width", "20%");
-    graphicArea.append("div").attr("id", "right-column").style("width", "80%");
+    //Create Divs:
+    graphicArea.append("div").attr("id", "leftColumn").style("width", "20%");
+    graphicArea.append("div").attr("id", "rightColumn").style("width", "80%");
+    graphicArea.style("display", "flex");
 
-    url_api_base = "/api/program_analysis/";
+    //Select Divs
+    let leftColumn = d3.select("#leftColumn");
+    let rightColumn = d3.select("#rightColumn");
+
+    //Add DropDown for what (Rev-Exp)
+    let menuRevExp = leftColumn.append("select").attr("id", "ddRevExp")
+    .style("width", "90%")
+    .style("font-size", "16px")
+    .style("margin-top", "15px");
+
+    menuRevExp.append("option").attr("value", "Revenue").text("Revenue");
+    menuRevExp.append("option").attr("value", "Expense").text("Expense");
+
+    //Add DropDown for Program Selection
+    let menuProgram = leftColumn.append("select").attr("id", "ddProg")
+    .style("width", "90%")
+    .style("font-size", "16px")
+    .style("margin-top", "15px");
+    
+    menuProgram.append("option").attr("value", "").text("Select a Program");
+
+    //URL
+    url_api_base = "/api/program_analysis/2023";
+    //Get API info to Populate DropDown
+    getJson(url_api_base).then(function(data){
+        
+        for (let looper1 = 0; looper1 < data.length; looper1++){
+            menuProgram.append("option").attr("value", data[looper1].Program).text(data[looper1].Program);
+        }
+        //Call Listeners 
+        handleRevExpSelection();
+        handleProgramSelection();
+    });
+
+    //start bar graph info with Default options
+    let revList = [0, 0, 0, 0, 0];
     let yearList = ["2019","2020","2021","2022","2023"];
-    let revList = [150000, 1851351, 138131, 813218, 215872];
-
-    // for (i=0; i < yearList.length; i++){
-    //     url = url_api_base + yearList[i];
-    //     console.log(url);
-    // }
-
-    graphicArea.append("div").attr("id", "left-column").style("width", "20%");
-    graphicArea.append("div").attr("id", "right-column").style("width", "80%");
-
-    let menu = graphicArea.append("select");
-
-
-    //start bar graph info
     let traceBar = [{
         x : yearList,
         y : revList,
@@ -58,28 +85,87 @@ function yourFunctionName(){
             }
         };
         //plot bargraph into bar id.
-        Plotly.newPlot("right-column", traceBar, layoutBar);
+        Plotly.newPlot("rightColumn", traceBar, layoutBar);
       
 }
 
+// Function to handle the dropdown menu selection for RevExp
+function handleRevExpSelection() {
+    const ddRevExp = document.getElementById("ddRevExp");
+    ddRevExp.addEventListener('change', function() {
+        const selectedOption = ddRevExp.value;
+        generateGraphics();
+    });
+}
 
-// // Create a dropdown menu structure
-// var dropdown = d3.select("#yourDivId")
-//     .append("select")
-//     .attr("id", "myDropdown");
+// Function to handle the dropdown menu selection for Program
+function handleProgramSelection() {
+    const ddProg = document.getElementById("ddProg");
+    ddProg.addEventListener('change', function() {
+        const selectedOption = ddProg.value;
+        generateGraphics();
+    });
+}
 
-// // Add options to the dropdown menu
-// var options = dropdown.selectAll("option")
-//     .data(["Option 1", "Option 2", "Option 3"])
-//     .enter()
-//     .append("option")
-//     .text(function(d) { return d; });
+function generateGraphics(){
+    let menuRevExp = d3.select("#ddRevExp");
+    let menuProg = d3.select("#ddProg");
 
-// // Add event listener to handle selection change
-// dropdown.on("change", function() {
-//     var selectedOption = d3.select(this).property("value");
-//     console.log("Selected option:", selectedOption);
-// });
+    let revexp = menuRevExp.property("value");
+    let prog = menuProg.property("value");
 
-    
-    
+    //console.log(revexp);
+    //console.log(prog);
+
+    let revList = [];
+    let expList = [];
+    let yearList = ["2019","2020","2021","2022","2023"];
+
+    url_api_base = "/api/program_analysis/";
+
+    for (let yearloop = 0; yearloop < yearList.length; yearloop++){
+        url = url_api_base + yearList[yearloop];
+        getJson(url).then(function(data){
+        
+            for (let plooper = 0; plooper < data.length; plooper++){
+                if(data[plooper].Program == prog){
+                    revList[yearloop] = data[plooper].rev
+                    expList[yearloop] = data[plooper].exp
+                }
+            }
+
+            if(yearList[yearloop] == "2023"){
+                
+                let yValues = []
+
+                console.log(prog);
+                console.log(yearList[yearloop]);
+                console.log(revList);
+                console.log(expList);
+
+                if(revexp == "Revenue"){
+                    yValues = revList;
+                }else{
+                    yValues = revList;
+                }
+
+                let traceBar = [{
+                    x : yearList,
+                    y : yValues,
+                    text : yValues,
+                    type : "bar",
+                    hoverinfo : "text"
+                    }];
+                    //start bargraph layout
+                    let layoutBar = {
+                    yaxis: {
+                        //tickvals: yValues, 
+                        //ticktext: yValues
+                        }
+                    };
+                    //plot bargraph into bar id.
+                    Plotly.newPlot("rightColumn", traceBar, layoutBar);    
+            }
+        });
+    }
+}
